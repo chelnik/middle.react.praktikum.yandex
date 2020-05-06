700486f54310d64fe5ca0de1ad8606536f6a0ba2
Vadim Chelnik <sindbadseafarer@yandex.ru>
2020-05-06 09:31:20 +0300

    back to work 2

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -1,38 +1,16 @@
 
 import React from 'react';
-import { render } from '../../node_modules/@types/testing-library__react';
-import { super } from '../../node_modules/@babel/types';
-
-class Gak extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {username: 'Ravil Kazumov'}
-  }
-  render{
-    const {username} = this.state
-    return(
-      <div>
-        {username}
-      </div>
-    );
-  }
-}
-
+import Comment from "./Comment";
+import {comment} from './Comment'
 function MyInfo(){
     return(
       <div>
-        <h1>Vadim Chelnik</h1>
-        <p>this is my story</p>
-        <ul>
-          <li>i was born in yeisk</li>
-          <li>five years old i leave my home</li>
-          <li>and today i programming my own web-site</li>
-          <li>Hi, bro. Ты лучший.</li>
-          <li><Gak/></li>
-        </ul>
+        <Comment
+          data={comment.data}
+          author={comment.author}/>
       </div>
     )
   }
   
 
-export default MyInfo;
+export default MyInfo; 
