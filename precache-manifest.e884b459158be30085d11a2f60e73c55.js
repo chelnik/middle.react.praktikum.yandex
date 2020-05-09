3019530deafdb3e5f55e609ec71eebb210a3c227
Vadim Chelnik <sindbadseafarer@yandex.ru>
2020-05-09 16:34:25 +0300

    Updates

---
+++ precache-manifest.e884b459158be30085d11a2f60e73c55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17c82d5f138ade6dfb0cb524c266d48",
+    "url": "/middle.react.praktikum.yandex/index.html"
+  },
+  {
+    "revision": "424d2d5aefeecf2ba92b",
+    "url": "/middle.react.praktikum.yandex/static/css/main.e50a9a2b.chunk.css"
+  },
+  {
+    "revision": "223c7b22a46669820ad2",
+    "url": "/middle.react.praktikum.yandex/static/js/2.ba08f40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/middle.react.praktikum.yandex/static/js/2.ba08f40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424d2d5aefeecf2ba92b",
+    "url": "/middle.react.praktikum.yandex/static/js/main.b45b5afb.chunk.js"
+  },
+  {
+    "revision": "c531594405f582b1dc9b",
+    "url": "/middle.react.praktikum.yandex/static/js/runtime-main.87182ac1.js"
+  }
+]);
